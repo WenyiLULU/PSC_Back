9bf5f382aa0478d8020564ffb2ce68737dcf19ce
Sebastian Marambio <hu.weee@gmail.com>
2022-06-23 18:08:45 +0200

    Co-authored-by: estebanba <estebanba@users.noreply.github.com> Co-authored-by: WenyiLULU <WenyiLULU@users.noreply.github.com>

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
-//const bcryptjs = require('bcryptjs')
-//const jwt = require('jsonwebtoken')
-//const { isAuthenticated } = require('../middlewares/jwt.middleware')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { isAuthenticated } = require('../middlewares/jwt.middleware')
 
 const User = require('../models/User.model')
 
@@ -8,44 +8,48 @@ const router = require('express').Router()
 
 router.post('/signup', async (req, res, next) => {
   const { username, email, password, country, city } = req.body
+  
+  if (!username || !email || !password || !country ||!city) {
+    res.status(403).json({ message: 'All fields are mandatory. Please fill them all.' });
+    return;
+  }
+  const newUser = { username, email, country, city}
 
-  const newUser = { username, email, passwordHashed: password, country, city}
+  const randomSalt = bcryptjs.genSaltSync(10)
 
-  //const randomSalt = bcryptjs.genSaltSync(10)
+  const passwordHash = bcryptjs.hashSync(password, randomSalt)
 
-  //const passwordHash = bcryptjs.hashSync(password, randomSalt)
-
-  //newUser.passwordHash = passwordHash
+  newUser.passwordHash = passwordHash
   try {
     await User.create(newUser)
     res.status(201).json({ message: 'New user created', status: 'OK' })
   } catch (error) {
     console.log(error)
     if (error.code === 11000) {
-      res.status(403).json({ message: 'Username already in use', status: 'KO' })
+      res.status(403).json({ message: 'Email is already in use', status: 'KO' })
     } else {
       res.status(500).json({ message: error, status: 'KO' })
     }
   }
 })
 
-/*router.post('/login', async (req, res, next) => {
-  const { username, password } = req.body
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body
 
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ email })
 
   if (user === null) {
-    res.status(404).json({ message: 'Username not found', status: 'KO' })
+    res.status(404).json({ message: 'User not found', status: 'KO' })
   } else {
     const { passwordHash, createdAt, updatedAt } = user
     if (bcryptjs.compareSync(password, passwordHash)) {
       const tempUser = { username, createdAt, updatedAt }
-      delete tempUser.passwordHash
+      //delete tempUser.passwordHash
       const token = jwt.sign(tempUser, process.env.TOKEN_SECRET, {
         algorithm: 'HS256',
         expiresIn: '6h',
       })
-      res.json({ token })
+      res.status(200).json({ token })
     } else {
       res.status(403).json({ message: 'Wrong password', status: 'KO' })
     }
@@ -62,6 +66,6 @@ router.get('/verify', isAuthenticated, (req, res, next) => {
   // Send back the object with user data
   // previously set as the token payload
   res.status(200).json(req.payload)
-})*/
+})
 
 module.exports = router
