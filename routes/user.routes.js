e7b56ad241e48cf7eae79a3afef8302832e0aba4
estebanba <esteban.ba@gmail.com>
2022-06-29 10:08:26 +0200

    add pet info to components

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const bcryptjs = require("bcryptjs");
+const fileUploader = require("../config/cloudinary.config");
 
 // get user infos by id
 router.get("/:userId", async (req, res, next) => {
@@ -15,26 +16,52 @@ router.get("/:userId", async (req, res, next) => {
 });
 
 // update user profile
-router.put('/:userId', fileUploader.single("userPhoto"), async (req, res, next) => {
-    const { userId }= req.params
-    console.log("req.body", req.body)
-    const { username, email, country, city, owner, sitter, pets, description, experience, image } = req.body
-    const newData = { username, email, country, city, owner, sitter, pets, description, experience, image }
-    console.log("req.file", req.file)
-    if(req.file){
-      newData.image = req.file.path
+router.put(
+  "/:userId",
+  fileUploader.single("userPhoto"),
+  async (req, res, next) => {
+    const { userId } = req.params;
+    console.log("req.body", req.body);
+    const {
+      username,
+      email,
+      country,
+      city,
+      owner,
+      sitter,
+      pets,
+      description,
+      experience,
+      image,
+    } = req.body;
+    const newData = {
+      username,
+      email,
+      country,
+      city,
+      owner,
+      sitter,
+      pets,
+      description,
+      experience,
+      image,
+    };
+    console.log("req.file", req.file);
+    if (req.file) {
+      newData.image = req.file.path;
     }
-    console.log("new data:", newData)
-  try {
-    const user = await User.findByIdAndUpdate(userId, newData);
+    console.log("new data:", newData);
+    try {
+      const user = await User.findByIdAndUpdate(userId, newData);
 
-    res
-      .status(201)
-      .json({ message: "User profile updated", name: user.username });
-  } catch (error) {
-    res.status(500).json(error);
+      res
+        .status(201)
+        .json({ message: "User profile updated", name: user.username });
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
-});
+);
 
 // change password
 router.put("/:userId/password", async (req, res, next) => {
