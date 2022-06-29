c13a237bb71e27f52c13b45533613eafbe90feb5
estebanba <esteban.ba@gmail.com>
2022-06-29 18:52:27 +0200

    add dashboard style

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,52 +1,53 @@
-const Appointments = require('../models/Appointment.model')
+const Appointments = require("../models/Appointment.model");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
 //get all
-router.get('/', async (req, res, next) => {
-    const allApp = await Appointments.find().populate('participant')
-    // allApp.populate('participant')
-    console.log('all:', allApp)
-    res.json(allApp)
-  })
+router.get("/", async (req, res, next) => {
+  const allApp = await Appointments.find().populate("creator pets");
+  // allApp.populate('participant')
+  console.log("all:", allApp);
+  res.json(allApp);
+});
 
 //get specific
-router.get('/:appointID', async (req, res, next) => {
-    const { appointID } = req.params
-  
-    const app = await Appointments.findById(appointID)
-    res.json(app)
-  })
+router.get("/:appointID", async (req, res, next) => {
+  const { appointID } = req.params;
+
+  const app = await Appointments.findById(appointID).populate(
+    "creator participant"
+  );
+  res.json(app);
+});
 
 //create new
-router.post('/create', async (req, res, next) => {
-    try {
-      const app = await Appointments.create(req.body)
-  
-      res.status(201).json({ message: 'New appointment created', id: app.id })
-    } catch (error) {
-      res.status(500).json(error)  
-    }
-  })
+router.post("/create", async (req, res, next) => {
+  try {
+    const app = await Appointments.create(req.body);
+
+    res.status(201).json({ message: "New appointment created", id: app.id });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //update
-router.put('/:appointID', async (req, res, next) => {
-    const {appointID} = req.params
-    try {
-        const app = await Appointments.findByIdAndUpdate(appointID, req.body)
-        res.status(201).json({ message: 'New appointment created', id: app.id })
-      } catch (error) {
-        res.status(500).json(error)
-      }
-})
+router.put("/:appointID", async (req, res, next) => {
+  const { appointID } = req.params;
+  try {
+    const app = await Appointments.findByIdAndUpdate(appointID, req.body);
+    res.status(201).json({ message: "New appointment created", id: app.id });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //delete
-router.delete('/:appointID', async (req, res, next) => {
-    const { appointID } = req.params
-  
-    await Appointments.findByIdAndDelete(appointID)
-    res.status(200).json({ message: 'Appointment deleted' })
-  })
+router.delete("/:appointID", async (req, res, next) => {
+  const { appointID } = req.params;
 
+  await Appointments.findByIdAndDelete(appointID);
+  res.status(200).json({ message: "Appointment deleted" });
+});
 
 module.exports = router;
